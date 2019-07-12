94a8269cf3c7f2b484f39be5413790ac13665735
Adrien <adrien.coudray@yahoo.fr>
2019-07-12 16:04:43 +0100

    updated controller to generate numbers bqsed on the current page

--- lib/fizzbuzz_phoenix_web/controllers/page_controller.ex
+++ lib/fizzbuzz_phoenix_web/controllers/page_controller.ex
@@ -10,8 +10,10 @@ defmodule FizzbuzzPhoenixWeb.PageController do
   @pages_nb div(@mex_size, @page_size)
 
   def index(conn, _params) do
-
-    sequence = Enum.map(1..100000000000, &fetch_or_generate_fizzbuzz_element/1)
+    current_page = get_page(_params, "page", 1)
+    first_number_of_current_page = 1 + (current_page-1)*@page_size
+    last_number_of_current_page = @page_size + first_number_of_current_page
+    sequence = Enum.map(first_number_of_current_page..last_number_of_current_page, &fetch_or_generate_fizzbuzz_element/1)
 
 
     render(conn, "index.html")
@@ -20,7 +22,6 @@ defmodule FizzbuzzPhoenixWeb.PageController do
   defp fetch_or_generate_fizzbuzz_element(number) do
 
     current_element = FizzBuzz.FizzBuzzElement |> FizzBuzz.Repo.get_by(number: number)
-
     cond do
       current_element == nil ->
        FizzBuzz.Repo.insert(%FizzBuzz.FizzBuzzElement{ number: number, fizz_buzz_value: FizzBuzz.fizzbuzz(number), favourited: false})
@@ -28,6 +29,17 @@ defmodule FizzbuzzPhoenixWeb.PageController do
       current_element
     end
 
+  end
 
+  defp get_page(params, name, default) do
+    %{^name => value} = params
+
+    case Integer.parse value do
+      {int, _rem} -> if int > 0, do: int, else: default
+      :error -> default
+    end
+
+  rescue
+    MatchError -> default
   end
 end
